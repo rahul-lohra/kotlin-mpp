1e98d714001f64f7c405457ba52230ed9b06b780
Rahul Lohra <tgunix@gmail.com>
2020-05-04 00:44:06 +0530

    1. add sqldelight - unstable build

---
+++ app/src/main/java/com/rahullohra/db/Storage.kt
@@ -0,0 +1,9 @@
+package com.rahullohra.db
+
+import com.squareup.sqldelight.db.SqlDriver
+
+class Storage {
+    fun createDb(driver: SqlDriver){
+
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/rahullohra/datasource/commonDb.kt
@@ -0,0 +1,8 @@
+package com.rahullohra.datasource
+
+import com.squareup.sqldelight.db.SqlDriver
+
+//expect fun driver():SqlDriver
+
+//class DbOperations {
+//}
